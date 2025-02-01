eab73540dcaf2ece2559c6e5726fb83cd5ee7e27
RedeComercial <luisinho1982@gmail.com>
2025-02-01 01:49:37 +0000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -21,3 +21,27 @@ function login() {
 }
 
 // Outras funções para registrar venda, listar vendas, etc.
+function registrarVenda() {
+  const formulario = document.getElementById('formulario-venda');
+  const dadosVenda = {
+    data: formulario.data.value,
+    cliente: formulario.cliente.value,
+    tipoAparelho: formulario['tipo-aparelho'].value,
+    valorAparelho: formulario['valor-aparelho'].value,
+    dataPagamento: formulario['data-pagamento'].value,
+    dataEntrega: formulario['data-entrega'].value
+  };
+
+  fetch('/vendas/registrar', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dadosVenda)
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('mensagem-registro').textContent = data.mensagem;
+    formulario.reset(); // Limpar o formulário após o registro
+  });
+}
